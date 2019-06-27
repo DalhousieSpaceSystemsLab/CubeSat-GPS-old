1cdd56b7209f4b820c97edb50d64dd52c2676521
Kaylee Kim <ar725001@dal.ca>
2019-06-27 18:58:01 -0300

    changed gps_data struct to contain static variables and decode method to use pointers

--- Source/source/GPS.cpp
+++ Source/source/GPS.cpp
@@ -25,8 +25,8 @@ string poll() {
 }
 
 //send in a NMEA string, get organized gps_data
-gps_data decode(string raw) { 
-	gps_data data = {};
+void decode(string raw) {
+	unique_ptr<gps_data> data;
 	minmea_sentence_gga ggaFrame;
 	minmea_sentence_rmc rmcFrame;
 	minmea_sentence_gll gllFrame;
@@ -41,37 +41,44 @@ gps_data decode(string raw) {
 		while (getline(ss, to, '\n')) {
 			string sentence = to;
 			if (minmea_parse_gga(&ggaFrame, sentence.c_str())) {
-				data.altitudeMeters = minmea_tofloat(&ggaFrame.altitude);
-				data.latitude = minmea_tocoord(&ggaFrame.latitude);
-				data.longitude = minmea_tocoord(&ggaFrame.longitude);
-				data.time_stamp = toStringTime(&ggaFrame.time);
+				data->altitudeMeters = minmea_tofloat(&ggaFrame.altitude);
+				data->latitude = minmea_tocoord(&ggaFrame.latitude);
+				data->longitude = minmea_tocoord(&ggaFrame.longitude);
+				data->time_stamp = toStringTime(&ggaFrame.time);
 			}
 			else if (minmea_parse_rmc(&rmcFrame, sentence.c_str())) {
-				data.latitude = minmea_tocoord(&rmcFrame.latitude);
-				data.longitude = minmea_tocoord(&rmcFrame.longitude);
-				data.time_stamp = toStringTime(&rmcFrame.time);
-				data.speedKnots =  minmea_tofloat(&rmcFrame.speed);
+				data->latitude = minmea_tocoord(&rmcFrame.latitude);
+				data->longitude = minmea_tocoord(&rmcFrame.longitude);
+				data->time_stamp = toStringTime(&rmcFrame.time);
+				data->speedKnots =  minmea_tofloat(&rmcFrame.speed);
 			}
 			else if (minmea_parse_gll(&gllFrame, sentence.c_str())) {
-				data.latitude = minmea_tocoord(&gllFrame.latitude);
-				data.longitude = minmea_tocoord(&gllFrame.longitude);
-				data.time_stamp = toStringTime(&gllFrame.time);
+				data->latitude = minmea_tocoord(&gllFrame.latitude);
+				data->longitude = minmea_tocoord(&gllFrame.longitude);
+				data->time_stamp = toStringTime(&gllFrame.time);
 			}
 			else if (minmea_parse_gst(&gstFrame, sentence.c_str())) {
-				data.time_stamp = toStringTime(&gstFrame.time);
+				data->time_stamp = toStringTime(&gstFrame.time);
 			}
 			else if (minmea_parse_vtg(&vtgFrame, sentence.c_str())) {
-				data.speedKnots = minmea_tofloat(&vtgFrame.speed_knots);
+				data->speedKnots = minmea_tofloat(&vtgFrame.speed_knots);
 			}
 			else if (minmea_parse_zda(&zdaFrame, sentence.c_str())) {
-				data.time_stamp = toStringTime(&zdaFrame.time);
+				data->time_stamp = toStringTime(&zdaFrame.time);
 			}
 		}
 	}
 	printf("time: %s\naltitude: %f\n(%f, %f)\nspeed: %f\n\n",
-		data.time_stamp.c_str(), data.altitudeMeters, data.latitude, data.longitude, data.speedKnots);
-	
-	return data;
+		data->time_stamp.c_str(), data->altitudeMeters, data->latitude, data->longitude, data->speedKnots);
+}
+
+void resetData() {
+	unique_ptr<gps_data> data;
+	data->time_stamp = "";
+	data->altitudeMeters = 0;
+	data->latitude = 0;
+	data->longitude = 0;
+	data->speedKnots = 0;
 }
 
 string toStringTime(struct minmea_time *time) {
@@ -108,16 +115,19 @@ vector<string> read_nmea_from_file() {
 
 //scheduler class to send messages
 int main() {
-    vector<string> nmea_data;
+	vector<string> nmea_data;
     
 	decode("abc"); //should be false
+	resetData();
 	decode("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"); //should be true
-	
+	resetData();
 	nmea_data = read_nmea_from_file();
 	cout << "\n" << "Read data from file: " << nmea_data[0] << "\n";
 	decode(nmea_data[0]);
 	decode("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47\n$GPZDA,201530.00,04,07,2002,00,00*60");
+	resetData();
 	decode("$GPZDA,201530.00,04,07,2002,00,00*60");
+	resetData();
 	stringstream paragraph;
 	paragraph << "$GPRMC,002454,A,3553.5295,N,13938.6570,E,0.0,43.1,180700,7.1,W,A*3F\n";
 	paragraph << "$GPGGA,023042,3907.3837,N,12102.4684,W,1,04,2.3,507.3,M,-24.1,M,,*75";
@@ -129,7 +139,9 @@ int main() {
 	paragraph << "$GPGLL, 3553.5295, N, 13938.6570, E, 002454, A, A * 4F";
 	paragraph << "$GPRMC, 002456, A, 3553.5295, N, 13938.6570, E, 0.0, 43.1, 180700, 7.1, W, A * 3D";
 	decode(paragraph.str());
+	resetData();
 	decode("$GPVTG, 054.7, T, 034.4, M, 005.5, N, 010.2, K * 48");
+	resetData();
 }
 
 
--- Source/source/GPS.hpp
+++ Source/source/GPS.hpp
@@ -6,19 +6,25 @@
 
 using namespace std;
 struct gps_data {
-    string time_stamp;
-    float latitude;
-    float longitude;
-    float altitudeMeters;
-	float speedKnots;
+    static string time_stamp;
+    static float latitude;
+    static float longitude;
+    static float altitudeMeters;
+	static float speedKnots;
 };
+string gps_data::time_stamp = "";
+float gps_data::latitude = 0;
+float gps_data::longitude = 0;
+float gps_data::altitudeMeters = 0;
+float gps_data::speedKnots = 0;
 
 bool gpsOn;
 
 int main();
 bool init_gps();
 string poll();
-gps_data decode(string raw);
+void decode(string raw);
+void resetData();
 string get_message();
 bool send_message(gps_data decoded_data);
 vector<string> read_nmea_from_file();
