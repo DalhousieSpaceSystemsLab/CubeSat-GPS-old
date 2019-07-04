b688db201ec07f8975853cd57f2e8bb9fa237e7e
Kaylee Kim <ar725001@dal.ca>
2019-07-04 20:00:36 -0300

    check gps data and persist getting data if insufficient data received

--- Source/source/GPS.cpp
+++ Source/source/GPS.cpp
@@ -20,10 +20,10 @@ bool init_gps() {
 
 //polls the GPS, gets data if turned on
 string poll() {
-	if (gpsOn) {
+	if (init_gps()) {
 		return get_message();
 	}
-	return NULL;
+	return "";
 }
 
 //send in a NMEA string, get organized gps_data
@@ -52,6 +52,28 @@ gps_data decode(string raw) {
 	return data;
 }
 
+bool check_gps_data(gps_data data) {
+	if (data.altitude == 0 || data.height == 0 ||
+		data.latitude < -90 || data.latitude > 90 ||
+		data.longitude < -180 || data.longitude > 180) {
+		return false;
+	}
+	return true;
+}
+
+gps_data persist() {
+	gps_data data = decode(poll());
+	bool success = check_gps_data(data);
+	while (!success) {
+		cout << "\n" << "INSUFFICIENT GPS DATA: RETRYING IN 5 SECONDS..." << "\n\n";
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+		data = decode(poll());
+		success = check_gps_data(data);
+	}
+	cout << "\n" << "SUFFICIENT GPS DATA: SENDING DATA..." << "\n\n";
+	return data;
+}
+
 string toStringTime(struct minmea_time *time) {
 	stringstream timeString;
 	timeString << time->hours << ":" << time->minutes << ":" << time->seconds << ":" << time->microseconds;
@@ -59,7 +81,19 @@ string toStringTime(struct minmea_time *time) {
 }
 
 string get_message() {
-	string message; // set message
+	// set message
+	string message;
+	if (gps_data::count == 0) {
+		message = "abc";
+	}
+	else if (gps_data::count == 1) {
+		message = "$GPGGA,184911.211,4523.444,N,06055.151,W,1,12,1.0,0.0,M,0.0,M,,*7D";
+	}
+	else if (gps_data::count == 2) {
+		message = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47";
+	}
+	gps_data::count++;
+	cout << "\n" << message << "\n";
 	return message;
 }
 
@@ -72,7 +106,7 @@ bool send_message(gps_data decoded_data) {
 	container.AddKeyValuePair(data_types.longitude, decoded_data.longitude);
 	//container.AddKeyValuePair(data_types.time_stamp, decoded_data.time_stamp);
 	container.AddKeyValuePair(data_types.height, decoded_data.height);
-	container.AddKeyValuePair(0, decoded_data.altitude);
+	container.AddKeyValuePair(data_types.altitude, decoded_data.altitude); //0?
 	
 
 	messageBuilder.SetMessageContents(container);
@@ -130,15 +164,17 @@ vector<string> read_nmea_from_file() {
 
 //scheduler class to send messages
 int main() {
+	send_message(persist());
+	/*
 	vector<string> nmea_data;
-    
+	
 	nmea_data = read_nmea_from_file();
 	cout << "\n" << "Read data from file: " << nmea_data[0] << "\n";
 //	send_message(decode(nmea_data[0]));
 	decode(nmea_data[0]);
 	
 	send_message(decode("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"));
-
+	
 	decode("$GPGGA,012219,1237.038,N,01531.000,E,1,08,0.9,125.4,M,46.9,M,,*47\n$GPZDA,201530.00,04,07,2002,00,00*60");
 	stringstream paragraph;
 	paragraph << "$GPGGA,023042,4007.3837,N,12102.89684,W,1,04,2.3,507.3,M,-24.1,M,,*75\n";
@@ -146,7 +182,7 @@ int main() {
 	paragraph << "$GPGGA,022454,3553.5295,N,13938.6570,E,1,05,2.2,18.3,M,39.0,M,,*7F\n";
 
 	decode(paragraph.str());
-
+	*/
 }
 
 
